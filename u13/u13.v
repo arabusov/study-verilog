f72a1e64103f9907859c9d8afdde59e9ddc8e3a7
Andrei Rabusov <arabusov@gmail.com>
2024-06-25 22:26:16 +0200

    Start implementing a bit of 6502

---
+++ u13/u13.v
@@ -0,0 +1,146 @@
+module alu(
+    input   cin
+    , input [MSBEXE:0] exe
+    , input [7:0] a
+    , input [7:0] b
+    , output reg cout
+    , output reg [7:0] c
+    );
+    parameter MSBEXE = 4;
+    always @(*)
+        case (exe)
+            0: {cout, c} = a + b + cin;
+            default: {cout, c} = {cin, a};
+        endcase
+endmodule
+
+module u13(
+    input clk
+    , input rst
+    , inout [7:0] data
+    , output reg [15:0] addr
+    , output reg rw
+    );
+    reg [15:0] pc;
+
+    parameter MSBEXE = 4;
+    reg [MSBEXE:0] alu_exe;
+    reg [3:0] state;
+    parameter FETCH_STATE = 0;
+    parameter DECODE_STATE = 1;
+    parameter ZP_STATE = 2;
+
+    always @(posedge clk) begin
+        if (rst)
+            reset;
+        else begin
+            case (state)
+                FETCH_STATE: fetch;
+                DECODE_STATE: decode;
+                ZP_STATE: zp;
+            endcase
+        end
+    end
+
+    parameter RST_ADDR = 16'hfff0;
+
+    reg [7:0] instr;
+
+    task reset;
+        begin
+            rw <= 0;
+            addr <= RST_ADDR;
+            pc <= RST_ADDR;
+            state <= FETCH_STATE;
+            carry <= 0;
+        end
+    endtask
+
+    task fetch;
+        begin
+            pc <= pc + 1;
+            addr <= pc + 1;
+            instr <= data;
+            state <= DECODE_STATE;
+        end
+    endtask
+
+    wire [3:0] ih, il;
+    assign {ih, il} = instr;
+    parameter NOP   = 8'hea;
+
+    // Main instructions
+    parameter ADC   = 4'h6;
+    parameter STA   = 4'h8;
+    parameter LDA   = 4'ha;
+
+    // Addressing modes
+    parameter IMMED = 4'h9;
+    parameter ZP    = 4'h5;
+
+    reg [7:0] a, b;
+    reg carry;
+    wire cout;
+    wire [7:0] c;
+
+    alu i_alu(.exe(alu_exe), .a(a), .b(b), .c(c), .cin(carry), .cout(cout));
+
+    task decode;
+        begin
+            if (instr == NOP)
+                fetch;
+            else begin
+                case (il)
+                    ZP: begin
+                        state <= ZP_STATE;
+                        addr <= { 8'h00, data };
+                    end
+                    IMMED: begin
+                        casez (ih)
+                            ADC: alu_t;
+                            LDA: lda_t;
+                            default: fetch;
+                        endcase
+                    end
+                    default: fetch;
+                endcase
+            end
+            case (ih)
+                ADC: alu_exe <= 0;
+                STA: rw <= 1;
+            endcase
+        end
+    endtask
+
+    task zp;
+        casez (ih)
+            ADC: alu_t;
+            LDA: lda_t;
+            STA: sta_t;
+        endcase
+    endtask
+
+    task lda_t;
+        begin
+            a <= data;
+            state <= FETCH_STATE;
+        end
+    endtask
+
+    assign data = ( (rw == 1) ? a : { 8{1'bz} } ); 
+
+    task sta_t;
+        begin
+            rw <= 0;
+            state <= FETCH_STATE;
+        end
+    endtask
+
+    task alu_t;
+        begin
+            b <= data;
+            {carry, a} <= {cout, c};
+            fetch;
+        end
+    endtask
+endmodule
---
+++ u13/u13_tb.v
@@ -0,0 +1,56 @@
+module ram8x16(d, a, we, clk);
+    inout [7:0] d;
+    input [15:0] a;
+    input we;
+    input clk;
+
+    reg [7:0] mem [0:15];
+    reg [7:0] zp [0:15];
+
+    assign out = a < 16'h100 ? zp[a[3:0]] : mem[a[3:0]];
+    assign d = ( we ? 16'bzzzzzzzzzzzzzzzz : mem[a[3:0]] );
+    always @(posedge clk)
+        if (we)
+            if (a < 16'h100)
+                zp[a[3:0]] <= d;
+            else
+                mem[a[3:0]] <= d;
+endmodule
+
+module u13_tb;
+
+reg clk, rst;
+wire [7:0] data;
+wire [15:0] addr;
+wire rw;
+
+u13 proc(.clk(clk), .rst(rst),
+    .data(data), .addr(addr),
+    .rw(rw));
+ram8x16 mem(.d(data), .a(addr), .we(rw), .clk(clk));
+initial $readmemh("mem.txt", mem.mem, 4'h0, 4'hf);
+
+integer i;
+always #1 clk = !clk;
+initial begin
+    $dumpfile("show_u13_tb.vcd");
+    $dumpvars(0, clk);
+    $dumpvars(0, rst);
+    $dumpvars(0, proc);
+    $dumpvars(0, mem);
+    $dumpvars(0, mem.mem[4'he]);
+    $display("your program:");
+    for (i = 4'h0; i <= 4'hf; i++) begin
+        $display(mem.mem[i]);
+    end
+    clk = 0;
+    rst = 1;
+    #5;
+    rst = 0;
+    while (addr < 16'hfffe && ~proc.carry) begin
+        #1;
+    end
+    $finish ;
+end
+
+endmodule
